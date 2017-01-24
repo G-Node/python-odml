64b0bae2d150274f9d4f102dd299d0722c8c0d68
Achilleas Koutsou <achilleas.k@gmail.com>
2017-01-24 18:34:00 +0100

    [setup] Adding install requires and README
    enum dependency only for < 3.4

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
-#!/usr/bin/env python
-
 import sys
-import os
-import glob
-
 try:
     from setuptools import setup
 except ImportError as ex:
     from distutils.core import setup
-
-kwargs = {}
 
 packages = [
     'odml',
     'odml.tools'
 ]
 
+with open('README.rst') as f:
+    description_text = f.read()
+
+install_req = ["lxml"]
+if sys.version_info < (3, 4):
+    install_req += ["enum"]
 
 setup(name='odML',
-      version='1.2',
+      version='1.3.dev0',
       description='open metadata Markup Language',
       author='Hagen Fritsch',
       author_email='fritsch+gnode@in.tum.de',
       url='http://www.g-node.org/projects/odml',
       packages=packages,
       test_suite='test',
-      **kwargs)
+      install_requires=install_req,
+      long_description=description_text,
+      )
