80468e340752300259d0c87fb6b677a62e7511dc
Jan Grewe <jan.grewe@g-node.org>
2017-02-10 11:36:50 +0100

    [test] some stubs for property tests

--- test/test_property.py
+++ test/test_property.py
@@ -5,11 +5,16 @@ from odml import Value, Property, Section, Document
 from odml.tools.xmlparser import XMLReader, XMLWriter
 from IPython import embed
 
+
 class TestProperty(unittest.TestCase):
 
+    def setUp(self):
+        pass
+
     def test_value(self):
+
         v = Value("A value")
-        p = Property(v)
+        p = Property("property", v)
         assert(p.value == v)
 
     def test_name(self):
@@ -23,3 +28,8 @@ class TestProperty(unittest.TestCase):
 
     def test_path(self):
         pass
+
+if __name__ == "__main__":
+    print("TestProperty")
+    tp = TestProperty()
+    tp.test_value()
