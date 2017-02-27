35977448f50d2f3d9e403fc832173bfbc151d0f9
Mehmet Suzen <msuzen@gmail.com>
2017-02-27 18:01:23 +0100

    Extenting odml.doc.BaseDocument to handle multiple Section/Property appends.

---
+++ odml/mdoc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import odml
+
+# Extent odml.doc.BaseDocument
+class odml_mdocument(odml.doc.BaseDocument):
+    def __init__(self,  author=None, date=None, version=None, repository=None):
+        odml.doc.BaseDocument.__init__(self, author, date, version, repository) 
+
+    def mappend(self, *obj_tuple):
+        ll = len(obj_tuple)
+        for i in range(0,ll):
+            self.append(obj_tuple[i])
+
