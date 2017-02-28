11b67e87d420dfb35d53a49f6e1523398c165278
Jan Grewe <jan.grewe@g-node.org>
2017-02-28 18:12:46 +0100

    [value] remove value from format.py, todo at dumping of values
    small edits

--- odml/format.py
+++ odml/format.py
@@ -35,13 +35,15 @@ class Property(Format):
     _name = "property"
     _args = {
         'name': 1,
-        'value': 1,
+        # 'value': 0,
+        'unit': 0,
         'definition': 0,
         'dependency': 0,
-        'dependencyvalue': 0
+        'dependencyvalue': 0,
+        'uncertainty': 0,
+        'value_reference': 0,
         }
     _map = {
-        'value': 'value',
         'dependencyvalue': 'dependency_value',
         'type': 'dtype'
         }
