0d943d920904ecb7c8d2fbb54bc9e5b71afdb900
yshalivskyy <yaroslav@shalivsky.com>
2017-03-30 16:58:30 +0300

    Add tests to cover removal and assigning functionality of parent.setter

--- test/test_property.py
+++ test/test_property.py
@@ -1,6 +1,7 @@
 import unittest
 from odml import Property, Section, Document
 from odml.section import BaseSection
+from odml.property import BaseProperty
 
 
 class TestProperty(unittest.TestCase):
@@ -19,6 +20,14 @@ class TestProperty(unittest.TestCase):
         p = Property("property_section", parent=Section("S"))
         self.assertIsInstance(p.parent, BaseSection)
         self.assertEqual(len(p.parent._props), 1)
+
+        """ test if child is removed from _props of a parent after assigning a new parent to the child """
+        prop_parent = p.parent
+        p.parent = Section("S1")
+        self.assertEqual(len(prop_parent._props), 0)
+        self.assertIsInstance(p.parent, BaseSection)
+        self.assertIsInstance(p.parent._props[0], BaseProperty)
+
         with self.assertRaises(ValueError):
             Property("property_prop", parent=Property("P"))
         with self.assertRaises(ValueError):
--- test/test_section.py
+++ test/test_section.py
@@ -17,14 +17,32 @@ class TestSection(unittest.TestCase):
     def test_parent(self):
         s = Section("Section")
         self.assertIsNone(s.parent)
+        s.parent = None
+        self.assertIsNone(s.parent)
+        s.parent = Document()
+        self.assertIsInstance(s.parent, BaseDocument)
+        self.assertIsInstance(s.parent._sections[0], BaseSection)
+
+        """ test if child is removed from _sections of a parent after assigning a new parent to the child """
+        p = s.parent
+        s.parent = Section("S")
+        self.assertEqual(len(p._sections), 0)
 
         s = Section("section_doc", parent=Document())
         self.assertIsInstance(s.parent, BaseDocument)
         self.assertEqual(len(s.parent._sections), 1)
+        p = s.parent
+        s.parent = None
+        self.assertEqual(len(p._sections), 0)
+        self.assertEqual(s.parent, None)
 
         s = Section("section_sec", parent=Section("S"))
         self.assertIsInstance(s.parent, BaseSection)
         self.assertEqual(len(s.parent._sections), 1)
+        p = s.parent
+        s.parent = None
+        self.assertEqual(len(p._sections), 0)
+        self.assertEqual(s.parent, None)
 
         with self.assertRaises(ValueError):
             Section("section_property", parent=Property("P"))
