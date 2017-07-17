2e3fd5cdd038b961a51727773d2ce6bcb674f5f3
yshalivskyy <yaroslav@shalivsky.com>
2017-07-17 18:42:49 +0300

    [rdf] add rdflib to setup.py and travis.yml

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ packages = [
 with open('README.rst') as f:
     description_text = f.read()
 
-install_req = ["lxml", "pyyaml"]
+install_req = ["lxml", "pyyaml", "rdflib"]
+
 if sys.version_info < (3, 4):
     install_req += ["enum34"]
 
