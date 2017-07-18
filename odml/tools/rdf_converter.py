81cc66c20d20629c7051b0d6243832c5795213a3
yshalivskyy <yaroslav@shalivsky.com>
2017-07-18 16:03:54 +0300

    [rdf] bug fixes and structure improvements

--- odml/tools/rdf_converter.py
+++ odml/tools/rdf_converter.py
@@ -1,21 +1,21 @@
 import sys
 import uuid
 
-from rdflib import Graph, BNode, Literal, URIRef
+from rdflib import Graph, Literal, URIRef
 from rdflib.namespace import XSD, RDF
 
 import odml
-import odml.format as format
+import odml.format
 
 try:
     unicode = unicode
 except NameError:
     unicode = str
 
-odmlns = format.Format.namespace()
+odmlns = odml.format.Format.namespace()
 
 
-class RDFWriter:
+class RDFWriter(object):
     """ 
     Creates the RDF graph storing information about the odML document 
     """
@@ -46,7 +46,7 @@ class RDFWriter:
         """
         Save the current element to the RDF graph
         :param e: current element 
-        :param node: Blank node to pass the earlier created node to inner elements
+        :param node: A node to pass the earlier created node to inner elements
         :return: the RDF graph 
         """
         fmt = e._format
@@ -56,17 +56,17 @@ class RDFWriter:
         else:
             curr_node = node
 
-        self.g.add((curr_node, fmt.rdf_type(), URIRef(fmt.rdf_type())))
+        self.g.add((curr_node, RDF.type, URIRef(fmt.rdf_type())))
 
         # adding doc to the hub
-        if isinstance(fmt, format.Document.__class__):
+        if isinstance(fmt, odml.format.Document.__class__):
             self.g.add((self.hub_root, odmlns.hasDocument, curr_node))
 
         for k in fmt._rdf_map:
             if k == 'id':
                 continue
-            elif (isinstance(fmt, format.Document.__class__) or
-                      isinstance(fmt, format.Section.__class__)) and k == "repository":
+            elif (isinstance(fmt, odml.format.Document.__class__) or
+                      isinstance(fmt, odml.format.Section.__class__)) and k == "repository":
                 terminology_url = getattr(e, k)
                 if terminology_url is None or not terminology_url:
                     continue
@@ -80,25 +80,26 @@ class RDFWriter:
                     self.g.add((self.hub_root, odmlns.hasTerminology, node))
                     self.g.add((curr_node, fmt.rdf_map(k), node))
             # generating nodes for entities: sections, properties and bags of values
-            elif (isinstance(fmt, format.Document.__class__) or
-                      isinstance(fmt, format.Section.__class__)) and \
+            elif (isinstance(fmt, odml.format.Document.__class__) or
+                      isinstance(fmt, odml.format.Section.__class__)) and \
                             k == 'sections' and len(getattr(e, k)) > 0:
                 sections = getattr(e, k)
                 for s in sections:
                     node = URIRef(odmlns + str(s.id))
                     self.g.add((curr_node, fmt.rdf_map(k), node))
                     self.save_element(s, node)
-            elif isinstance(fmt, format.Section.__class__) and \
+            elif isinstance(fmt, odml.format.Section.__class__) and \
                             k == 'properties' and len(getattr(e, k)) > 0:
                 properties = getattr(e, k)
                 for p in properties:
                     node = URIRef(odmlns + str(p.id))
                     self.g.add((curr_node, fmt.rdf_map(k), node))
                     self.save_element(p, node)
-            elif isinstance(fmt, format.Property.__class__) and \
+            elif isinstance(fmt, odml.format.Property.__class__) and \
                             k == 'value' and len(getattr(e, k)) > 0:
                 values = getattr(e, k)
-                bag = BNode(str(uuid.uuid4()))
+                bag = URIRef(str(uuid.uuid4()))
+                self.g.add((bag, RDF.type, RDF.Bag))
                 self.g.add((curr_node, fmt.rdf_map(k), bag))
                 for v in values:
                     self.g.add((bag, RDF.li, Literal(v)))
@@ -109,7 +110,8 @@ class RDFWriter:
                     continue
                 elif k == 'date':
                     self.g.add((curr_node, fmt.rdf_map(k), Literal(val, datatype=XSD.date)))
-                self.g.add((curr_node, fmt.rdf_map(k), Literal(val)))
+                else:
+                    self.g.add((curr_node, fmt.rdf_map(k), Literal(val)))
         return self.g
 
     def _get_terminology_by_value(self, url):
@@ -130,12 +132,3 @@ class RDFWriter:
         f = open(filename, "w")
         f.write(data)
         f.close()
-
-
-if __name__ == "__main__":
-    l1 = "./python-odml/doc/example_odMLs/ex_1.odml"
-    l2 = "./python-odml/doc/example_odMLs/ex_2.odml"
-    o1 = odml.load(l1)
-    o2 = odml.load(l2)
-    r = RDFWriter([o1, o2], "hub1")
-    r.write_file("./ex_1.rdf")
