9075ca0af9af861841bc7a02c32ae7091ab112e0
Achilleas Koutsou <achilleas.k@gmail.com>
2017-08-14 14:16:24 +0200

    [setup] Update info for v1.3 release

--- setup.py
+++ setup.py
@@ -16,14 +16,15 @@ install_req = ["lxml", "pyyaml"]
 if sys.version_info < (3, 4):
     install_req += ["enum"]
 
-setup(name='odML',
-      version='1.3.dev0',
-      description='open metadata Markup Language',
-      author='Hagen Fritsch',
-      author_email='fritsch+gnode@in.tum.de',
-      url='http://www.g-node.org/projects/odml',
-      packages=packages,
-      test_suite='test',
-      install_requires=install_req,
-      long_description=description_text,
-      )
+setup(
+    name='odML',
+    version='1.3.0',
+    description='open metadata Markup Language',
+    author='G-Node',
+    author_email='dev@g-node.org',
+    url='http://www.g-node.org/projects/odml',
+    packages=packages,
+    test_suite='test',
+    install_requires=install_req,
+    long_description=description_text,
+)
