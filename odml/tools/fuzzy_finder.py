4f4af41fbe9109eaf5d64bd76a9a50b94f7a4719
yshalivskyy <yaroslav@shalivsky.com>
2017-10-15 16:16:17 +0300

    [rdf] fixes and structure improvements for FuzzyFinder

--- odml/tools/fuzzy_finder.py
+++ odml/tools/fuzzy_finder.py
@@ -1,7 +1,4 @@
-import pprint
-import time
-
-from odml.tools.query_creator import QueryCreator, QueryParser, QueryParser2
+from odml.tools.query_creator import QueryCreator, QueryParser, QueryParserFuzzy
 
 
 class FuzzyFinder(object):
@@ -16,77 +13,81 @@ class FuzzyFinder(object):
         self.prepared_queries_list = []
         self._subsets = []
 
-    def find(self, graph=None, q_str=None, q_params=None):
+    def find(self, mode='fuzzy', graph=None, q_str=None, q_params=None):
+        # TODO warn users if they added non-odml attributes ('naming' instead of 'name' e.g.)
         """
         Apply set of queries to the graph and returns info that was retrieved from queries.
         
-        :param graph: graph object
+        :param mode:     define the type of parser which will be used for parsing parameters or queries.
+                         Please find our more info about concrete parsers in odml/tool/query_creator.py or tutorials.
+        :param graph:    graph object.
+        :param q_str:    query string which used in QueryCreator class.
+                         Example for QueryParser: doc(author:D. N. Adams) section(name:Stimulus) prop(name:Contrast, value:20, unit:%)
+                         Example for QueryParserFuzzy: "FIND sec(name) prop(type) HAVING Stimulus, Contrast"
         :param q_params: dictionary object with set of parameters for a query
-                         Example: {'Sec': [('name', 'Stimulus')],
-                                   'Doc': [('author', 'D. N. Adams')],
-                                   'Prop': [('name', 'Contrast'), ('value':[20]), ('unit':'%')]}
-        :param q_str: query string which used in QueryCreator class
-                      Example: doc(author:D. N. Adams) section(name:Stimulus) prop(name:Contrast, value:20, unit:%)
-        :return: string which contains set of triples
+                         Example for QueryParser: {'Sec': [('name', 'Stimulus')],
+                                                   'Doc': [('author', 'D. N. Adams')],
+                                                   'Prop': [('name', 'Contrast'), ('value':[20, 25]), ('unit':'%')]}
+                         Example for QueryParserFuzzy: {'Sec': ['name', 'type'],
+                                                        'Doc': ['author'],
+                                                        'Search': ['Stimulus', 'Contrast']}
+        :return:         string which contains set of triples.
         """
-        self._validate_find_input_attributes(graph, q_str, q_params, QueryParser())
+        if mode == 'fuzzy':
+            q_parser = QueryParserFuzzy()
+            pairs_generator = self._generate_parameters_pairs_fuzzy
+        elif mode == 'match':
+            q_parser = QueryParser()
+            pairs_generator = self._generate_parameters_pairs
+        else:
+            raise ValueError("Parameter mode can be either 'fuzzy' or 'match'")
 
-        self._generate_parameters_subsets(self._generate_parameters_pairs())
+        self._validate_find_input_attributes(graph, q_str, q_params, q_parser)
 
-        return self._output_query_results()
-
-    def find_fuzzy(self, graph=None, q_str=None, q_params=None):
-        """
-        :param q_params: dictionary object with set of parameters for a query
-               Example: {'Sec': ['name', 'type'],
-                         'Doc': ['author'],
-                         'Search': ['Stimulus', 'Contrast']}
-        :param q_str: query string which used in QueryCreator class
-               Example: "select sec(name) prop(type) where Stimulus, Contrast"
-        :param graph: graph object
-        :return: string which contains set of triples
-        """
-        self._validate_find_input_attributes(graph, q_str, q_params, QueryParser2())
-
-        self._generate_parameters_subsets(self._generate_parameters_pairs_fuzzy())
+        self._generate_parameters_subsets(pairs_generator())
 
         return self._output_query_results()
 
     def _validate_find_input_attributes(self, graph, q_str, q_params, q_parser):
-        if not graph:
+        if not graph and not self.graph:
+            raise ValueError("Please provide a RDF graph")
+
+        if not self.graph:
             self.graph = graph
+
         if q_str and q_params:
-            raise ValueError("Please pass query parameters only as string or dict object")
+            raise ValueError("Please pass query parameters only as a string or a dict object")
 
         if q_str:
             self.q_params = q_parser.parse_query_string(q_str)
         elif q_params:
             self.q_params = q_params
         else:
-            raise ValueError("Please pass query parameters either as string or dict object")
+            raise ValueError("Please pass query parameters either as a string or a dict object")
 
     def _generate_parameters_pairs(self):
         """
-        {'Sec': [('name', 'some_name'), ('type', 'Stimulus')]}
+        Example: {'Sec': [('name', 'some_name'), ('type', 'Stimulus')]}
         :return: [('Sec', ('name', 'some_name')), ('Sec', ('type', 'Stimulus'))]
         """
-        attrs = []
+        parameters_pairs = []
         possible_keys = QueryCreator.possible_q_dict_keys
         for key in possible_keys:
             if key in self.q_params.keys():
                 object_attrs = self.q_params[key]
                 for object_attr in object_attrs:
                     s = tuple([key, object_attr])
-                    attrs.append(s)
-        return attrs
+                    parameters_pairs.append(s)
+        return parameters_pairs
 
     def _generate_parameters_pairs_fuzzy(self):
         """
         Generates set of tuples matching search select and where parts of fuzzy finder query
         from dictionary of parameters.
-        Example: {'Sec': ['name', 'type'],
-                  'Doc': ['author'],
-                  'Search': ['Stimulus', 'Contrast']} 
+        
+        Example:  {'Sec': ['name', 'type'],
+                   'Doc': ['author'],
+                   'Search': ['Stimulus', 'Contrast']} 
         :return:  [('Sec', ('name', 'Stimulus')), ('Sec', ('name', 'Contrast')), 
                    ('Sec', ('type', 'Stimulus')), ('Sec', ('name', 'Contrast')),
                    ('Doc', ('author', 'Stimulus')), ('Doc', ('author', 'Contrast'))]
@@ -114,15 +115,15 @@ class FuzzyFinder(object):
             self._subsets_util_dfs(0, [], self._subsets, sorted(attrs))
 
         self._subsets.sort(key=len, reverse=True)
-        pprint.pprint(self._subsets)  # debug statement, left to explicitly show the list of subsets
 
     def _subsets_util_dfs(self, index, path, res, attrs):
         """
-        Generates all subsets of attrs set using Depth-first search
+        Generates all subsets of attrs set using Depth-first search.
         Example (with numbers for explicity: [1,2,3] -> [[1], [2], [3], [1,2], [1,3], [2,3], [1,2,3]]
-        :param index: help index for going through list
-        :param path: array for saving subsets
-        :param res: result subset
+        
+        :param index: help index for going through list.
+        :param path:  array for saving subsets.
+        :param res:   result subset.
         :param attrs: input list of attrs e.g. [('Sec', ('name', 'some_name')), ('Sec', ('type', 'Stimulus'))]
         """
         if path:
@@ -131,7 +132,8 @@ class FuzzyFinder(object):
             if self._check_duplicate_attrs(path, attrs[i]):
                 self._subsets_util_dfs(i + 1, path + [attrs[i]], res, attrs)
 
-    def _check_duplicate_attrs(self, attrs_list, attr):
+    @staticmethod
+    def _check_duplicate_attrs(attrs_list, attr):
         for i in attrs_list:
             if attr[1][0] == i[1][0]:
                 return False
@@ -150,29 +152,30 @@ class FuzzyFinder(object):
             if triples:
                 output_triples_string += creator.query
                 output_triples_string += triples
+                output_triples_string += '\n'
+
         return output_triples_string
 
     def _execute_query(self, query):
         """
-        Execute prepared query on the graph
+        Execute prepared query on the graph.
+        
         :param query: prepared query object
         :return: string with output triples
         """
-        t0 = time.perf_counter()
         output_string = ""
         for row in self.graph.query(query):
             row_string = self._build_output_str(row)
             output_string += row_string
-        t1 = time.perf_counter()
-        print('Execution time: ', t1-t0)  # left for debug and benchmarking
         return output_string
 
     @staticmethod
     def _build_output_str(row):
         """
-        Build output string depending on the query variables
-        :param row: rdflib query row
-        :return: string with values
+        Build output string depending on the query variables.
+        
+        :param row: rdflib query row.
+        :return: string with values.
         """
         out_str = ""
         possible_vars = QueryCreator.possible_query_variables
@@ -188,10 +191,11 @@ class FuzzyFinder(object):
     @staticmethod
     def _prepare_query(args):
         """
-        Return a query for given parameters
+        Return a query for given parameters.
+        
         :param args: dict with list of odML object attributes for creation query
                      Example: {'Sec': [('name', 'some_name'), ('type', 'Stimulus')]}
-        :return: QueryCreator object
+        :return: QueryCreator object.
         """
         q_params = {}
         for arg in args:
