f0f0baf23fcc7b248a6c60608dd0aa7fb57871f8
M. Sonntag <michael.p.sonntag@gmail.com>
2017-10-16 16:59:47 +0200

    [tools] make version converter available

--- odml/tools/__init__.py
+++ odml/tools/__init__.py
@@ -2,4 +2,5 @@ from odml.tools import dumper
 from odml.tools import odmlparser
 from odml.tools import jsonparser
 from odml.tools import rdf_converter
+from odml.tools import version_converter
 from odml.tools import xmlparser
