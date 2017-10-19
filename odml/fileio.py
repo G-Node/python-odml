ba2eeaf16cddd97dac58f87f8a4492b0132b69c7
Jan Grewe <jan.grewe@g-node.org>
2017-10-19 10:35:29 +0200

    [fileio] add proper error if trying to load non-existing file

--- odml/fileio.py
+++ odml/fileio.py
@@ -1,9 +1,12 @@
+import os
 from .tools.odmlparser import ODMLReader, ODMLWriter
 
 parsers = ["xml", "json", "yaml"]
 
 
 def load(filename, backend="xml"):
+    if not os.path.exists(filename):
+        raise FileNotFoundError("File \'%s\' was not found!" % (filename if len(filename) < 20 else "...%s" % filename[19:]))
     reader = ODMLReader(backend)
     return reader.from_file(filename)
 
