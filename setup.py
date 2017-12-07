c9c4dd627183754f30343c12aa5b053d20bf2b0d
M. Sonntag <michael.p.sonntag@gmail.com>
2017-12-07 11:23:13 +0100

    [init/setup] Update packaging information
    Added author list, contact, classifiers, license text
    and updated the provided hompage for future packaging.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ try:
     from setuptools import setup
 except ImportError as ex:
     from distutils.core import setup
+from odml.info import AUTHOR, CONTACT, CLASSIFIERS, HOMEPAGE, VERSION
 
 packages = [
     'odml',
@@ -12,19 +13,24 @@ packages = [
 with open('README.rst') as f:
     description_text = f.read()
 
+with open("LICENSE") as f:
+    license_text = f.read()
+
 install_req = ["lxml", "pyyaml"]
 if sys.version_info < (3, 4):
     install_req += ["enum34"]
 
 setup(
     name='odML',
-    version='1.3.2',
+    version=VERSION,
     description='open metadata Markup Language',
-    author='G-Node',
-    author_email='dev@g-node.org',
-    url='http://www.g-node.org/projects/odml',
+    author=AUTHOR,
+    author_email=CONTACT,
+    url=HOMEPAGE,
     packages=packages,
     test_suite='test',
     install_requires=install_req,
     long_description=description_text,
+    classifiers=CLASSIFIERS,
+    license=license_text
 )
