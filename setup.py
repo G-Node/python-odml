00caa0a04306bb48d423bbd639d97715a2241863
M. Sonntag <michael.p.sonntag@gmail.com>
2018-02-20 16:39:24 +0100

    [setup] Quick fix local install dependency issue

--- setup.py
+++ setup.py
@@ -4,7 +4,25 @@ try:
     from setuptools import setup
 except ImportError as ex:
     from distutils.core import setup
-from odml.info import AUTHOR, CONTACT, CLASSIFIERS, HOMEPAGE, VERSION
+
+try:
+    from odml.info import AUTHOR, CONTACT, CLASSIFIERS, HOMEPAGE, VERSION
+except ImportError as ex:
+    # Read the information from odml.info.py if package dependencies
+    # are not yet available during a local install.
+    CLASSIFIERS = ""
+    with open('odml/info.py') as f:
+        for line in f:
+            curr_args = line.split(" = ")
+            if len(curr_args) == 2:
+                if curr_args[0] == "AUTHOR":
+                    AUTHOR = curr_args[1].replace('\'', '').replace('\\', '').strip()
+                elif curr_args[0] == "CONTACT":
+                    CONTACT = curr_args[1].replace('\'', '').strip()
+                elif curr_args[0] == "HOMEPAGE":
+                    HOMEPAGE = curr_args[1].replace('\'', '').strip()
+                elif curr_args[0] == "VERSION":
+                    VERSION = curr_args[1].replace('\'', '').strip()
 
 packages = [
     'odml',
