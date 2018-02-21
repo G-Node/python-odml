a1be88811bb0b3cb57b0c7791f2e31c43ccf02ac
M. Sonntag <michael.p.sonntag@gmail.com>
2018-02-21 19:17:15 +0100

    [info.py] Read information from info.json file

--- odml/info.py
+++ odml/info.py
@@ -1,15 +1,15 @@
-VERSION = '1.4.0'
-FORMAT_VERSION = '1.1'
-AUTHOR = 'Hagen Fritsch, Christian Kellner, Jan Grewe, ' \
-          'Achilleas Koutsou, Michael Sonntag, Lyuba Zehl'
-COPYRIGHT = '(c) 2011-2017, German Neuroinformatics Node'
-CONTACT = 'dev@g-node.org'
-HOMEPAGE = 'https://github.com/G-Node/python-odml'
-CLASSIFIERS = [
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: BSD License',
-    'Development Status :: 5 - Production/Stable',
-    'Topic :: Scientific/Engineering',
-    'Intended Audience :: Science/Research'
-]
+import os
+import json
+
+here = os.path.dirname(__file__)
+
+with open(os.path.join(here, "info.json")) as infofile:
+    infodict = json.load(infofile)
+
+VERSION = infodict["VERSION"]
+FORMAT_VERSION = infodict["FORMAT_VERSION"]
+AUTHOR = infodict["AUTHOR"]
+COPYRIGHT = infodict["COPYRIGHT"]
+CONTACT = infodict["CONTACT"]
+HOMEPAGE = infodict["HOMEPAGE"]
+CLASSIFIERS = infodict["CLASSIFIERS"]
