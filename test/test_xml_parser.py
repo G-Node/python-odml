13438e355120fdee01ef28c483db30ce9767cf05
M. Sonntag <michael.p.sonntag@gmail.com>
2018-02-27 17:40:21 +0100

    [test] xmlparser: Distinguish ParserExceptions

--- test/test_xml_parser.py
+++ test/test_xml_parser.py
@@ -1,5 +1,6 @@
-import unittest
 import os
+import unittest
+
 from odml.tools import xmlparser
 from odml.tools.parser_utils import ParserException
 
@@ -13,18 +14,27 @@ class TestXMLParser(unittest.TestCase):
 
     def test_invalid_root(self):
         filename = "invalid_root.xml"
+        message = "Expecting <odML>"
 
-        with self.assertRaises(ParserException):
+        with self.assertRaises(ParserException) as exc:
             _ = self.xml_reader.from_file(os.path.join(self.basepath, filename))
+
+        self.assertIn(message, str(exc.exception))
 
     def test_missing_version(self):
         filename = "missing_version.xml"
+        message = "Could not find format version attribute"
 
-        with self.assertRaises(ParserException):
+        with self.assertRaises(ParserException) as exc:
             _ = self.xml_reader.from_file(os.path.join(self.basepath, filename))
+
+        self.assertIn(message, str(exc.exception))
 
     def test_invalid_version(self):
         filename = "invalid_version.xml"
+        message = "invalid odML document format version"
 
-        with self.assertRaises(ParserException):
+        with self.assertRaises(ParserException) as exc:
             _ = self.xml_reader.from_file(os.path.join(self.basepath, filename))
+
+        self.assertIn(message, str(exc.exception))
