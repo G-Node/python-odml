3a2aebaceb9991275e719f33351f6eac1451a062
M. Sonntag <michael.p.sonntag@gmail.com>
2018-02-27 17:36:58 +0100

    [test] Add jsonparser main tag tests

--- test/test_json_parser.py
+++ test/test_json_parser.py
@@ -1,5 +1,7 @@
-import unittest
+import json
 import os
+import unittest
+
 from odml.tools import dict_parser
 from odml.tools.parser_utils import ParserException
 
@@ -10,3 +12,39 @@ class TestJSONParser(unittest.TestCase):
         self.basepath = 'test/resources/'
 
         self.json_reader = dict_parser.DictReader()
+
+    def test_missing_root(self):
+        filename = "missing_root.json"
+        message = "Missing root element"
+
+        with open(os.path.join(self.basepath, filename)) as json_data:
+            parsed_doc = json.load(json_data)
+
+        with self.assertRaises(ParserException) as exc:
+            _ = self.json_reader.to_odml(parsed_doc)
+
+        self.assertIn(message, str(exc.exception))
+
+    def test_missing_version(self):
+        filename = "missing_version.json"
+        message = "Could not find odml-version"
+
+        with open(os.path.join(self.basepath, filename)) as json_data:
+            parsed_doc = json.load(json_data)
+
+        with self.assertRaises(ParserException) as exc:
+            _ = self.json_reader.to_odml(parsed_doc)
+
+        self.assertIn(message, str(exc.exception))
+
+    def test_invalid_version(self):
+        filename = "invalid_version.json"
+        message = "invalid odML document format version"
+
+        with open(os.path.join(self.basepath, filename)) as json_data:
+            parsed_doc = json.load(json_data)
+
+        with self.assertRaises(ParserException) as exc:
+            _ = self.json_reader.to_odml(parsed_doc)
+
+        self.assertIn(message, str(exc.exception))
