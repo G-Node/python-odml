25a836af7bb1d4c4af3ba0e747e9a51bba6776d4
M. Sonntag <michael.p.sonntag@gmail.com>
2018-02-27 16:53:57 +0100

    [test] Add xmlparser main tag tests

--- test/test_xml_parser.py
+++ test/test_xml_parser.py
@@ -4,9 +4,27 @@ from odml.tools import xmlparser
 from odml.tools.parser_utils import ParserException
 
 
-class TestParser(unittest.TestCase):
+class TestXMLParser(unittest.TestCase):
 
     def setUp(self):
         self.basepath = 'test/resources/'
 
         self.xml_reader = xmlparser.XMLReader()
+
+    def test_invalid_root(self):
+        filename = "invalid_root.xml"
+
+        with self.assertRaises(ParserException):
+            _ = self.xml_reader.from_file(os.path.join(self.basepath, filename))
+
+    def test_missing_version(self):
+        filename = "missing_version.xml"
+
+        with self.assertRaises(ParserException):
+            _ = self.xml_reader.from_file(os.path.join(self.basepath, filename))
+
+    def test_invalid_version(self):
+        filename = "invalid_version.xml"
+
+        with self.assertRaises(ParserException):
+            _ = self.xml_reader.from_file(os.path.join(self.basepath, filename))
