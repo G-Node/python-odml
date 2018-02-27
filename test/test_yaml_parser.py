9de14eb5f2b2a576325c38781ed04e7b8504df25
M. Sonntag <michael.p.sonntag@gmail.com>
2018-02-27 17:49:31 +0100

    [test] Add yamlparser main tag tests

--- test/test_yaml_parser.py
+++ test/test_yaml_parser.py
@@ -12,3 +12,39 @@ class TestYAMLParser(unittest.TestCase):
         self.basepath = 'test/resources/'
 
         self.yaml_reader = dict_parser.DictReader()
+
+    def test_missing_root(self):
+        filename = "missing_root.yaml"
+        message = "Missing root element"
+
+        with open(os.path.join(self.basepath, filename)) as raw_data:
+            parsed_doc = yaml.load(raw_data)
+
+        with self.assertRaises(ParserException) as exc:
+            _ = self.yaml_reader.to_odml(parsed_doc)
+
+        self.assertIn(message, str(exc.exception))
+
+    def test_missing_version(self):
+        filename = "missing_version.yaml"
+        message = "Could not find odml-version"
+
+        with open(os.path.join(self.basepath, filename)) as raw_data:
+            parsed_doc = yaml.load(raw_data)
+
+        with self.assertRaises(ParserException) as exc:
+            _ = self.yaml_reader.to_odml(parsed_doc)
+
+        self.assertIn(message, str(exc.exception))
+
+    def test_invalid_version(self):
+        filename = "invalid_version.yaml"
+        message = "invalid odML document format version"
+
+        with open(os.path.join(self.basepath, filename)) as raw_data:
+            parsed_doc = yaml.load(raw_data)
+
+        with self.assertRaises(ParserException) as exc:
+            _ = self.yaml_reader.to_odml(parsed_doc)
+
+        self.assertIn(message, str(exc.exception))
