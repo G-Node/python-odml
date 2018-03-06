d153b8046f52a679898089990f390e61086ec472
Jan Grewe <jan.grewe@g-node.org>
2018-03-06 16:13:04 +0100

    [property] return value as tuple

--- odml/property.py
+++ odml/property.py
@@ -150,7 +150,7 @@ class BaseProperty(base.baseobject, Property):
 
     @property
     def value(self):
-        return self._value
+        return tuple(self._value)
 
     def value_str(self, index=0):
         """
