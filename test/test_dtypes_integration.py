46edbf66d0429912f13fcfffc766806f68fee6d6
M. Sonntag <michael.p.sonntag@gmail.com>
2018-03-16 18:17:06 +0100

    [test/dtype int] Add setUp and tearDown

--- test/test_dtypes_integration.py
+++ test/test_dtypes_integration.py
@@ -4,16 +4,32 @@ of all supported odML datatypes with all supported
 odML parsers.
 """
 
+import os
+import shutil
+import tempfile
 import unittest
+
+import odml
 
 
 class TestTypesIntegration(unittest.TestCase):
 
     def setUp(self):
-        pass
+        # Set up test environment
+        self.tmp_dir = tempfile.mkdtemp(suffix=".odml")
+
+        self.json_file = os.path.join(self.tmp_dir, "test.json")
+        self.xml_file = os.path.join(self.tmp_dir, "test.xml")
+        self.yaml_file = os.path.join(self.tmp_dir, "test.yaml")
+
+        # Set up odML document stub
+        doc = odml.Document()
+        _ = odml.Section(name="dtypes", type="test", parent=doc)
+        self.doc = doc
 
     def tearDown(self):
-        pass
+        if os.path.exists(self.tmp_dir):
+            shutil.rmtree(self.tmp_dir)
 
     def test_time(self):
         pass
