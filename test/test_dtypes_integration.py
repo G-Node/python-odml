63991f11c68ed1d389946ead13b5c3d6434bc490
M. Sonntag <michael.p.sonntag@gmail.com>
2018-03-16 17:57:12 +0100

    [test] Add dtype integration test file

---
+++ test/test_dtypes_integration.py
@@ -0,0 +1,40 @@
+"""
+This file tests proper creation, saving and loading
+of all supported odML datatypes with all supported
+odML parsers.
+"""
+
+import unittest
+
+
+class TestTypesIntegration(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_time(self):
+        pass
+
+    def test_date(self):
+        pass
+
+    def test_datetime(self):
+        pass
+
+    def test_int(self):
+        pass
+
+    def test_float(self):
+        pass
+
+    def test_str(self):
+        pass
+
+    def test_bool(self):
+        pass
+
+    def test_tuple(self):
+        pass
