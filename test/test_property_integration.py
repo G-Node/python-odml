254d2a06d490e49d51db0966b212262681dd0211
M. Sonntag <michael.p.sonntag@gmail.com>
2018-03-20 18:06:04 +0100

    [test/prop int] Add property integration test file

---
+++ test/test_property_integration.py
@@ -0,0 +1,31 @@
+"""
+This file tests proper creation, saving and loading
+of odML Properties with all supported odML parsers.
+"""
+
+import os
+import shutil
+import tempfile
+import unittest
+
+import odml
+
+
+class TestPropertyIntegration(unittest.TestCase):
+
+    def setUp(self):
+        # Set up test environment
+        self.tmp_dir = tempfile.mkdtemp(suffix=".odml")
+
+        self.json_file = os.path.join(self.tmp_dir, "test.json")
+        self.xml_file = os.path.join(self.tmp_dir, "test.xml")
+        self.yaml_file = os.path.join(self.tmp_dir, "test.yaml")
+
+        # Set up odML document stub
+        doc = odml.Document()
+        _ = odml.Section(name="properties", type="test", parent=doc)
+        self.doc = doc
+
+    def tearDown(self):
+        if os.path.exists(self.tmp_dir):
+            shutil.rmtree(self.tmp_dir)
