7410cc0fb8179d914b6fd44085b3848cfb486eff
M. Sonntag <michael.p.sonntag@gmail.com>
2018-03-20 18:07:31 +0100

    [test/prop int] Add test simple attributes

--- test/test_property_integration.py
+++ test/test_property_integration.py
@@ -71,3 +71,60 @@ class TestPropertyIntegration(unittest.TestCase):
         self.assertEqual(jdoc.sections[0].properties[prop_name].id, assigned_id)
         self.assertEqual(xdoc.sections[0].properties[prop_name].id, assigned_id)
         self.assertEqual(ydoc.sections[0].properties[prop_name].id, assigned_id)
+
+    def test_simple_attributes(self):
+        """
+        This test checks correct writing and loading of 'simple'
+        Property format attributes meaning all attributes that
+        do not require any special handling when they are set.
+
+        Note: the xml reader and writer converts all attribute values
+        to string whereas json and yaml retain other types like int or
+        float identity. Since this is currently not relevant, all
+        attributes are tested as string values.
+        """
+        p_name = "propertyName"
+        p_origin = "from over there"
+        p_unit = "pears"
+        p_uncertainty = "+-12"
+        p_ref = "4 8 15 16 23"
+        p_def = "an odml test property"
+        p_dep = "yes"
+        p_dep_val = "42"
+
+        _ = odml.Property(name=p_name, value_origin=p_origin, unit=p_unit,
+                          uncertainty=p_uncertainty, reference=p_ref, definition=p_def,
+                          dependency=p_dep, dependency_value=p_dep_val,
+                          parent=self.doc.sections[0])
+
+        jdoc, xdoc, ydoc = self.save_load()
+
+        # Test correct JSON save and load.
+        jprop = jdoc.sections[0].properties[p_name]
+        self.assertEqual(jprop.name, p_name)
+        self.assertEqual(jprop.value_origin, p_origin)
+        self.assertEqual(jprop.unit, p_unit)
+        self.assertEqual(jprop.uncertainty, p_uncertainty)
+        self.assertEqual(jprop.reference, p_ref)
+        self.assertEqual(jprop.dependency, p_dep)
+        self.assertEqual(jprop.dependency_value, p_dep_val)
+
+        # Test correct XML save and load.
+        xprop = xdoc.sections[0].properties[p_name]
+        self.assertEqual(xprop.name, p_name)
+        self.assertEqual(xprop.value_origin, p_origin)
+        self.assertEqual(xprop.unit, p_unit)
+        self.assertEqual(xprop.uncertainty, p_uncertainty)
+        self.assertEqual(xprop.reference, p_ref)
+        self.assertEqual(xprop.dependency, p_dep)
+        self.assertEqual(xprop.dependency_value, p_dep_val)
+
+        # Test correct YAML save and load.
+        yprop = ydoc.sections[0].properties[p_name]
+        self.assertEqual(yprop.name, p_name)
+        self.assertEqual(yprop.value_origin, p_origin)
+        self.assertEqual(yprop.unit, p_unit)
+        self.assertEqual(yprop.uncertainty, p_uncertainty)
+        self.assertEqual(yprop.reference, p_ref)
+        self.assertEqual(yprop.dependency, p_dep)
+        self.assertEqual(yprop.dependency_value, p_dep_val)
