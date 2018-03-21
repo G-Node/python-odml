686ad111b99d4f7255b4f2cc519b02af1551aef3
M. Sonntag <michael.p.sonntag@gmail.com>
2018-03-21 09:48:18 +0100

    [test/document] Update id test

--- test/test_doc.py
+++ test/test_doc.py
@@ -7,9 +7,16 @@ class TestSection(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_set_id(self):
-        d = Document("D", id="79b613eb-a256-46bf-84f6-207df465b8f7")
-        self.assertEqual(d.id, "79b613eb-a256-46bf-84f6-207df465b8f7")
+    def test_id(self):
+        doc = Document()
+        self.assertIsNotNone(doc.id)
 
-        Document("D", id="id")
-        self.assertNotEqual(d.id, "id")
+        doc = Document("D", id="79b613eb-a256-46bf-84f6-207df465b8f7")
+        self.assertEqual(doc.id, "79b613eb-a256-46bf-84f6-207df465b8f7")
+
+        doc = Document("D", id="id")
+        self.assertNotEqual(doc.id, "id")
+
+        # Make sure id cannot be reset programmatically.
+        with self.assertRaises(AttributeError):
+            doc.id = "someId"
