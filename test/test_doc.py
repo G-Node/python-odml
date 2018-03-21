064a95aa27ffa75f1aa7900aaab9b7d7be790166
M. Sonntag <michael.p.sonntag@gmail.com>
2018-03-21 09:50:35 +0100

    [test/document] Add new_id test

--- test/test_doc.py
+++ test/test_doc.py
@@ -20,3 +20,21 @@ class TestSection(unittest.TestCase):
         # Make sure id cannot be reset programmatically.
         with self.assertRaises(AttributeError):
             doc.id = "someId"
+
+    def test_new_id(self):
+        doc = Document()
+        old_id = doc.id
+
+        # Test assign new generated id.
+        doc.new_id()
+        self.assertNotEqual(old_id, doc.id)
+
+        # Test assign new custom id.
+        old_id = doc.id
+        doc.new_id("79b613eb-a256-46bf-84f6-207df465b8f7")
+        self.assertNotEqual(old_id, doc.id)
+        self.assertEqual("79b613eb-a256-46bf-84f6-207df465b8f7", doc.id)
+
+        # Test invalid custom id exception.
+        with self.assertRaises(ValueError):
+            doc.new_id("crash and burn")
