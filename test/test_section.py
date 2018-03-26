794650cfc969ebf5b9513d537120564c365e4f72
M. Sonntag <michael.p.sonntag@gmail.com>
2018-03-26 21:07:18 +0200

    [test/section] Add extend unique name test

--- test/test_section.py
+++ test/test_section.py
@@ -227,15 +227,15 @@ class TestSection(unittest.TestCase):
             main.append(Property(name="prop"))
 
     def test_extend(self):
-        sec = Section(name="sec")
+        sec = Section(name="main")
         self.assertListEqual(sec.sections, [])
         self.assertListEqual(sec.properties, [])
 
         # Test extend with Section list
-        sec.extend([Section(name="subsec1"), Section(name="subsec2")])
+        sec.extend([Section(name="sec1"), Section(name="sec2")])
         self.assertEqual(len(sec), 2)
         self.assertEqual(len(sec.sections), 2)
-        self.assertEqual(sec.sections[0].name, "subsec1")
+        self.assertEqual(sec.sections[0].name, "sec1")
 
         # Test extend with Property list
         sec.extend((Property(name="prop1"), Property(name="prop2")))
@@ -244,7 +244,7 @@ class TestSection(unittest.TestCase):
         self.assertEqual(sec.properties[0].name, "prop1")
 
         # Test extend with mixed list
-        sec.extend([Section(name="subsec3"), Property(name="prop3")])
+        sec.extend([Section(name="sec3"), Property(name="prop3")])
         self.assertEqual(len(sec), 6)
         self.assertEqual(len(sec.sections), 3)
         self.assertEqual(len(sec.properties), 3)
@@ -256,3 +256,12 @@ class TestSection(unittest.TestCase):
         # Test fail on non Section/Property list entry
         with self.assertRaises(ValueError):
             sec.extend([Property(name="prop4"), 5])
+
+        # Test fail on same name entities
+        with self.assertRaises(KeyError):
+            sec.extend([Property(name="new"), Property(name="prop3")])
+        self.assertEqual(len(sec.properties), 3)
+
+        with self.assertRaises(KeyError):
+            sec.extend([Section(name="new"), Section(name="sec3")])
+        self.assertEqual(len(sec.sections), 3)
