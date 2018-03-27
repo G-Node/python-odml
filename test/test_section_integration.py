7c51de43052bea56ce6f5339123ac3e0852b6012
M. Sonntag <michael.p.sonntag@gmail.com>
2018-03-27 15:04:08 +0200

    [test/section int] section integration test file

---
+++ test/test_section_integration.py
@@ -0,0 +1,30 @@
+"""
+This file tests proper creation, saving and loading
+of odML Sections with all supported odML parsers.
+"""
+
+import os
+import shutil
+import tempfile
+import unittest
+
+import odml
+
+
+class TestSectionIntegration(unittest.TestCase):
+
+    def setUp(self):
+        # Set up test environment
+        self.tmp_dir = tempfile.mkdtemp(suffix=".odml")
+
+        self.json_file = os.path.join(self.tmp_dir, "test.json")
+        self.xml_file = os.path.join(self.tmp_dir, "test.xml")
+        self.yaml_file = os.path.join(self.tmp_dir, "test.yaml")
+
+        # Set up odML document stub
+        doc = odml.Document()
+        self.doc = doc
+
+    def tearDown(self):
+        if os.path.exists(self.tmp_dir):
+            shutil.rmtree(self.tmp_dir)
