586a305fca7633e3ed2456aeb30673f25437eb29
M. Sonntag <michael.p.sonntag@gmail.com>
2018-04-13 19:55:32 +0200

    [test/odmlparser] Add RDF file test

--- test/test_parser_odml.py
+++ test/test_parser_odml.py
@@ -23,14 +23,17 @@ class TestOdmlParser(unittest.TestCase):
         self.json_file = os.path.join(self.tmp_dir, "test.json")
         self.xml_file = os.path.join(self.tmp_dir, "test.xml")
         self.yaml_file = os.path.join(self.tmp_dir, "test.yaml")
+        self.rdf_file = os.path.join(self.tmp_dir, "test.ttl")
 
         self.xml_reader = odmlparser.ODMLReader(parser='XML')
         self.yaml_reader = odmlparser.ODMLReader(parser='YAML')
         self.json_reader = odmlparser.ODMLReader(parser='JSON')
+        self.rdf_reader = odmlparser.ODMLReader(parser='RDF')
 
         self.xml_writer = odmlparser.ODMLWriter(parser='XML')
         self.yaml_writer = odmlparser.ODMLWriter(parser='YAML')
         self.json_writer = odmlparser.ODMLWriter(parser='JSON')
+        self.rdf_writer = odmlparser.ODMLWriter(parser='RDF')
 
         self.odml_doc = self.xml_reader.from_file(self.basefile)
 
@@ -59,3 +62,34 @@ class TestOdmlParser(unittest.TestCase):
         self.assertEqual(xml_doc, self.odml_doc)
         self.assertEqual(xml_doc, json_doc)
         self.assertEqual(xml_doc, yaml_doc)
+
+    def test_xml_file(self):
+        self.xml_writer.write_file(self.odml_doc, self.xml_file)
+        xml_doc = self.xml_reader.from_file(self.xml_file)
+
+        self.assertEqual(xml_doc, self.odml_doc)
+
+    def test_yaml_file(self):
+        self.yaml_writer.write_file(self.odml_doc, self.yaml_file)
+        yaml_doc = self.yaml_reader.from_file(self.yaml_file)
+
+        self.assertEqual(yaml_doc, self.odml_doc)
+
+    def test_json_file(self):
+        self.json_writer.write_file(self.odml_doc, self.json_file)
+        json_doc = self.json_reader.from_file(self.json_file)
+
+        self.assertEqual(json_doc, self.odml_doc)
+
+    def test_rdf_file(self):
+        self.rdf_writer.write_file(self.odml_doc, self.rdf_file)
+        rdf_doc = self.rdf_reader.from_file(self.rdf_file, "turtle")
+
+        # RDF does not preserve the order of sections,
+        # need to check the attributes by hand.
+        self.assertEqual(len(rdf_doc), 1)
+        self.assertEqual(rdf_doc[0].author, self.odml_doc.author)
+        self.assertEqual(rdf_doc[0].version, self.odml_doc.version)
+        self.assertEqual(rdf_doc[0].date, self.odml_doc.date)
+        self.assertIn(self.odml_doc.sections[0].name, rdf_doc[0].sections)
+        self.assertIn(self.odml_doc.sections[1].name, rdf_doc[0].sections)
