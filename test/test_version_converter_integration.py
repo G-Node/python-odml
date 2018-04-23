7c4d138856bfbd7625f40d99cd84076e947d1a79
M. Sonntag <michael.p.sonntag@gmail.com>
2018-04-23 22:12:27 +0200

    [test/versionConv int] Reduce redundant code

--- test/test_version_converter_integration.py
+++ test/test_version_converter_integration.py
@@ -32,37 +32,7 @@ class TestVersionConverterIntegration(unittest.TestCase):
         VC(self.xmlfile).write_to_file(self.outfile)
         self.assertTrue(os.path.exists(self.outfile))
 
-        doc = load(self.outfile)
-
-        repo = "http://portal.g-node.org/odml/terminologies/v1.1/terminologies.xml"
-        self.assertEqual(doc.repository, repo)
-        self.assertEqual(doc.author, "author")
-        self.assertEqual(doc.version, "v1.13")
-        self.assertEqual(len(doc.sections), 3)
-
-        sec = doc.sections["sec_one"]
-        self.assertEqual(sec.definition, "def s1")
-        self.assertEqual(sec.reference, "ref s1")
-        self.assertEqual(sec.type, "mainsec")
-        self.assertEqual(len(sec.sections), 1)
-        self.assertEqual(len(sec.properties), 3)
-
-        prop = sec.properties["prop_one"]
-        self.assertEqual(prop.definition, "def prop1")
-        self.assertEqual(prop.dependency, "dep p1")
-        self.assertEqual(prop.dependency_value, "dep val p1")
-        self.assertEqual(prop.reference, "ref val 1")
-        self.assertEqual(prop.uncertainty, "11")
-        self.assertEqual(prop.unit, "arbitrary")
-        self.assertEqual(prop.value_origin, "filename val 1")
-        self.assertEqual(prop.dtype, "string")
-        self.assertEqual(len(prop.value), 3)
-
-        prop = sec.properties["prop_two"]
-        self.assertEqual(len(prop.value), 8)
-
-        prop = sec.properties["prop_three"]
-        self.assertEqual(len(prop.value), 0)
+        self.check_result()
 
     def test_convert_yaml(self):
         with self.assertRaises(Exception):
@@ -71,38 +41,7 @@ class TestVersionConverterIntegration(unittest.TestCase):
         VC(self.yamlfile).write_to_file(self.outfile, "YAML")
         self.assertTrue(os.path.exists(self.outfile))
 
-        doc = load(self.outfile)
-
-        # Test document attribute export
-        self.assertEqual(doc.author, "author")
-        self.assertEqual(doc.version, "v1.13")
-        repo = "http://portal.g-node.org/odml/terminologies/v1.1/terminologies.xml"
-        self.assertEqual(doc.repository, repo)
-        self.assertEqual(len(doc.sections), 3)
-
-        sec = doc.sections["sec_one"]
-        self.assertEqual(sec.definition, "def s1")
-        self.assertEqual(sec.reference, "ref s1")
-        self.assertEqual(sec.type, "mainsec")
-        self.assertEqual(len(sec.sections), 1)
-        self.assertEqual(len(sec.properties), 3)
-
-        prop = sec.properties["prop_one"]
-        self.assertEqual(prop.definition, "def prop1")
-        self.assertEqual(prop.dependency, "dep p1")
-        self.assertEqual(prop.dependency_value, "dep val p1")
-        self.assertEqual(prop.reference, "ref val 1")
-        self.assertEqual(prop.uncertainty, "11")
-        self.assertEqual(prop.unit, "arbitrary")
-        self.assertEqual(prop.value_origin, "filename val 1")
-        self.assertEqual(prop.dtype, "string")
-        self.assertEqual(len(prop.value), 3)
-
-        prop = sec.properties["prop_two"]
-        self.assertEqual(len(prop.value), 8)
-
-        prop = sec.properties["prop_three"]
-        self.assertEqual(len(prop.value), 0)
+        self.check_result()
 
     def test_convert_json(self):
         with self.assertRaises(Exception):
@@ -111,6 +50,9 @@ class TestVersionConverterIntegration(unittest.TestCase):
         VC(self.jsonfile).write_to_file(self.outfile, "JSON")
         self.assertTrue(os.path.exists(self.outfile))
 
+        self.check_result()
+
+    def check_result(self):
         doc = load(self.outfile)
 
         # Test document attribute export
