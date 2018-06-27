aaa5586e1ee7a2d69f19a73c795e23f5eca53e83
M. Sonntag <michael.p.sonntag@gmail.com>
2018-06-27 12:12:39 +0200

    [setup] Use pyyaml 3.12
    Use pyyaml 3.12 for now, since pyyaml 4.1 seems
    to contain breaking changes.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ packages = [
 with open('README.rst') as f:
     description_text = f.read()
 
-install_req = ["lxml", "pyyaml", "rdflib"]
+install_req = ["lxml", "pyyaml==3.12", "rdflib"]
 
 if sys.version_info < (3, 4):
     install_req += ["enum34"]
