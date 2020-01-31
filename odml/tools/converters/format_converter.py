ee027f787907bed51bb285a90ed20bfd2211f045
M. Sonntag <michael.p.sonntag@gmail.com>
2020-01-31 12:56:45 +0100

    [FormatConv] Minor PEP8 fixes
    - invalid-name
    - trailing-whitespaces
    - line-too-long

--- odml/tools/converters/format_converter.py
+++ odml/tools/converters/format_converter.py
@@ -29,19 +29,16 @@ class FormatConverter(object):
     def convert(cls, args=None):
         """
         Enable usage of the argparse for calling convert_dir(...)
-        Example:
-            1) >> python -m odml.tools.converters.format_converter ./..path../input_dir v1_1 -out ./..path../output_dir -r
-            
-               Convert files from the path <./..path../input_dir> to .xml odml version 1.1,
-               writes them into <./..path../output_dir> including subdirectories 
-               and its files from the input path.
-            
-            2) >> python -m odml.tools.converters.format_converter ./..path../input_dir odml
-            
-               Converts files from path <./..path../input_dir> to .odml,
-               writes them into <./..path../input_dir_odml> not including subdirectories.
+        Examples:
+          1) >> python -m odml.tools.converters.format_converter <in_dir> v1_1 -out <out_dir> -r
+            Convert files from the path <in_dir> to .xml odml version 1.1, writes them
+            to <out_dir> including subdirectories and its files from the input path.
+          2) >> python -m odml.tools.converters.format_converter <in_dir> odml
+            Converts files from path <in_dir> to .odml and writes them
+            to <in_dir_odml> not including subdirectories.
         """
-        parser = argparse.ArgumentParser(description="Convert directory with odml files to another format")
+        desc = "Convert directory with odml files to another format"
+        parser = argparse.ArgumentParser(description=desc)
         parser.add_argument("input_dir", help="Path to input directory")
         parser.add_argument("result_format", choices=list(CONVERSION_FORMATS),
                             help="Format of output files")
@@ -49,21 +46,22 @@ class FormatConverter(object):
         parser.add_argument("-r", "--recursive", action="store_true",
                             help="Enable converting files from subdirectories")
         args = parser.parse_args(args)
-        r = True if args.recursive else False
-        cls.convert_dir(args.input_dir, args.output_dir, r, args.result_format)
+        recursive = True if args.recursive else False
+        cls.convert_dir(args.input_dir, args.output_dir, recursive, args.result_format)
 
     @classmethod
     def convert_dir(cls, input_dir, output_dir, parse_subdirs, res_format):
         """
         Convert files from given input directory to the specified res_format.
         :param input_dir: Path to input directory
-        :param output_dir: Path for output directory. If None, new directory will be created on the same level as input
+        :param output_dir: Path for output directory.
+                           If None, new directory will be created on the same level as input
         :param parse_subdirs: If True enable converting files from subdirectories
-        :param res_format: Format of output files. 
-                           Possible choices: "v1_1" (converts to version 1.1 from version 1 xml)
-                                             "odml" (converts to .odml from version 1.1 .xml files)
-                                             "turtle", "nt" etc. (converts to rdf formats from version 1.1 .odml files)
-                                             (see full list of rdf serializers in CONVERSION_FORMATS)
+        :param res_format: Format of output files.
+                           Possible choices: "v1_1" (version 1 to version 1.1 xml files)
+                                             "odml" (version 1.1 .xml to .odml files)
+                                             "turtle", "nt" etc. (version 1.1 to RDF files)
+                                             (full list of RDF serializers in CONVERSION_FORMATS)
         """
         if res_format not in CONVERSION_FORMATS:
             raise ValueError("Format for output files is incorrect. "
@@ -73,8 +71,9 @@ class FormatConverter(object):
         input_dir = os.path.join(input_dir, '')
 
         if output_dir is None:
+            # find the directory that contains input_dir
             input_dir_name = os.path.basename(os.path.dirname(input_dir))
-            root_dir = os.path.dirname(os.path.dirname(input_dir))  # find the directory that contains input_dir
+            root_dir = os.path.dirname(os.path.dirname(input_dir))
             output_dir_name = input_dir_name + "_" + res_format
             output_dir = os.path.join(root_dir, output_dir_name)
             cls._create_sub_directory(output_dir)
@@ -84,8 +83,8 @@ class FormatConverter(object):
         if not parse_subdirs:
             for file_name in os.listdir(input_dir):
                 if os.path.isfile(os.path.join(input_dir, file_name)):
-                    cls._convert_file(os.path.join(input_dir, file_name), os.path.join(output_dir, file_name),
-                                      res_format)
+                    cls._convert_file(os.path.join(input_dir, file_name),
+                                      os.path.join(output_dir, file_name), res_format)
         else:
             for dir_path, dir_names, file_names in os.walk(input_dir):
                 for file_name in file_names:
@@ -104,13 +103,13 @@ class FormatConverter(object):
             VersionConverter(input_path).write_to_file(output_path)
         elif res_format == "odml":
             if not output_path.endswith(".odml"):
-                p, _ = os.path.splitext(output_path)
-                output_path = p + ".odml"
+                file_path, _ = os.path.splitext(output_path)
+                output_path = file_path + ".odml"
             odml.save(odml.load(input_path), output_path)
         elif res_format in CONVERSION_FORMATS:
             if not output_path.endswith(CONVERSION_FORMATS[res_format]):
-                p, _ = os.path.splitext(output_path)
-                output_path = p + CONVERSION_FORMATS[res_format]
+                file_path, _ = os.path.splitext(output_path)
+                output_path = file_path + CONVERSION_FORMATS[res_format]
             RDFWriter(odml.load(input_path)).write_file(output_path, res_format)
         else:
             raise ValueError("Format for output files is incorrect. "
@@ -127,9 +126,7 @@ class FormatConverter(object):
     @staticmethod
     def _check_input_output_directory(input_dir, output_dir):
         """
-        Checks if provided directory is valid - not None, is directory and not a root folder in the File System 
-        if output dir was not provided.
-        Raise relevant exceptions.
+        Checks if the directories are valid.
         """
         if not input_dir or not os.path.isdir(input_dir):
             raise ValueError("The path to input directory is not a valid path")
@@ -139,8 +136,9 @@ class FormatConverter(object):
 
         if not output_dir:
             if os.path.dirname(input_dir) == input_dir:
-                raise ValueError("The input directory cannot be a root folder of the File System if "
-                                 "output directory was not specified")
+                msg = "Cannot write to %s. Please specify an output directory" % input_dir
+                raise ValueError(msg)
+
 
 if __name__ == "__main__":
     FormatConverter.convert(sys.argv[1:])
