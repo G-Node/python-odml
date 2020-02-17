70be7848cc130f89b967456886421ac07a9b4e59
M. Sonntag <michael.p.sonntag@gmail.com>
2020-02-17 18:16:33 +0100

    [tools/ParserUtils] Add odml tuple export func

--- odml/tools/parser_utils.py
+++ odml/tools/parser_utils.py
@@ -34,3 +34,24 @@ class InvalidVersionException(ParserException):
     Exception wrapper to indicate a non-compatible odML version.
     """
     pass
+
+
+def odml_tuple_export(odml_tuples):
+    """
+    Converts odml style tuples to a parsable string representation.
+    Every tuple is represented by brackets '()'. The individual elements of a tuple are
+    separated by a semicolon ';'. The individual tuples are separated by a comma ','.
+    An odml 3-tuple list of 2 tuples would be serialized to: "[(11;12;13),(21;22;23)]".
+
+    :param odml_tuples: List of odml style tuples.
+    :return: string
+    """
+    str_tuples = ""
+    for val in odml_tuples:
+        str_val = ";".join(val)
+        if str_tuples:
+            str_tuples = "%s,(%s)" % (str_tuples, str_val)
+        else:
+            str_tuples = "(%s)" % str_val
+
+    return "[%s]" % str_tuples
