f3afd7af8eba82ae8f260e3278386cdec7150c82
fschrader1992 <fschrader1992@gmail.com>
2020-03-24 22:26:10 +0100

    [test_rdf_reader.py] Add RDF Reader Tests For Doc, Section and Property

--- test/test_rdf_reader.py
+++ test/test_rdf_reader.py
@@ -42,3 +42,66 @@ class TestRDFReader(unittest.TestCase):
         self.assertEqual(len(r[0].sections), 1)
         self.assertEqual(len(r[0].sections[0].sections), 1)
         self.assertEqual(len(r[0].sections[0].properties), 1)
+
+    def test_doc(self):
+        """
+        Test if a document and its attributes get converted correctly from rdf to odml.
+        """
+        doc = Document()
+        doc.author = "D. N. Adams"
+        doc.version = 42
+        doc.date = datetime.date(1979, 10, 12)
+
+        w = RDFWriter(doc).get_rdf_str()
+        r = RDFReader().from_string(w, "turtle")
+
+        self.assertEqual(r[0].author, "D. N. Adams")
+        self.assertEqual(r[0].version, "42")
+        self.assertEqual(r[0].date, datetime.date(1979, 10, 12))
+
+    def test_section(self):
+        """
+        Test if a section and its attributes get converted correctly from rdf to odml.
+        """
+        doc = Document()
+        sec1 = Section(name="sec1", type="test", parent=doc, definition="Interesting stuff.",
+                       reference="The Journal")
+        Section(name="sec2", type="test", parent=sec1)
+
+        w = RDFWriter(doc).get_rdf_str()
+        r = RDFReader().from_string(w, "turtle")
+
+        self.assertEqual(r[0].sections[0].name, "sec1")
+        self.assertEqual(r[0].sections[0].type, "test")
+        self.assertEqual(r[0].sections[0].id, sec1.id)
+        self.assertEqual(r[0].sections[0].definition, "Interesting stuff.")
+        self.assertEqual(r[0].sections[0].reference, "The Journal")
+        self.assertEqual(r[0].sections[0].parent, r[0])
+        self.assertEqual(len(r[0].sections[0].sections), 1)
+
+    def test_property(self):
+        """
+        Test if a property and its attributes get converted correctly from rdf to odml.
+        """
+        doc = Document()
+        sec1 = Section(name="sec1", type="test", parent=doc)
+        prop2 = Property(name="numbers", definition="any number", dtype="float", parent=sec1,
+                         values=[1, 3.4, 67.8, -12], unit="meter", uncertainty=0.8,
+                         value_origin="force", reference="Experiment 1")
+
+        w = RDFWriter(doc).get_rdf_str()
+        r = RDFReader().from_string(w, "turtle")
+
+        prop = r[0].sections[0].properties["numbers"]
+
+        self.assertEqual(prop.name, "numbers")
+        self.assertEqual(prop.dtype, "float")
+        self.assertEqual(prop.id, prop2.id)
+        self.assertEqual(prop.parent, r[0].sections[0])
+        self.assertEqual(len(prop.values), 4)
+        self.assertEqual(prop.values, [1, 3.4, 67.8, -12])
+        self.assertEqual(prop.definition, "any number")
+        self.assertEqual(prop.unit, "meter")
+        self.assertEqual(prop.uncertainty, "0.8")
+        self.assertEqual(prop.value_origin, "force")
+        self.assertEqual(prop.reference, "Experiment 1")
