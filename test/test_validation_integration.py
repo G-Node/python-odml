89c8538ab68ff50cefb1947ab584fe61b2902aad
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-04 11:23:10 +0200

    [test/validation_int] Test prop values cardinality

--- test/test_validation_integration.py
+++ test/test_validation_integration.py
@@ -35,3 +35,81 @@ class TestValidationIntegration(unittest.TestCase):
 
         return out
 
+    def test_property_values_cardinality(self):
+        # -- Test assignment validation warnings
+        doc = odml.Document()
+        sec = odml.Section(name="sec", type="sec_type", parent=doc)
+
+        # -- Test cardinality validation warnings on Property init
+        # Test warning when setting invalid minimum
+        _ = odml.Property(name="prop_card_min", values=[1], val_cardinality=(2, None), parent=sec)
+        output = self._get_captured_output()
+        test_msg = "%s (minimum %s values, %s found)" % (self.msg_base, 2, 1)
+        self.assertEqual(len(output), 1)
+        self.assertIn(test_msg, output[0])
+
+        # Test warning when setting invalid maximum
+        _ = odml.Property(name="prop_card_max", values=[1, 2, 3], val_cardinality=2, parent=sec)
+        output = self._get_captured_output()
+        test_msg = "%s (maximum %s values, %s found)" % (self.msg_base, 2, 3)
+        self.assertEqual(len(output), 1)
+        self.assertIn(test_msg, output[0])
+
+        # Test no warning on valid init
+        prop_card = odml.Property(name="prop_card", values=[1, 2],
+                                  val_cardinality=(1, 5), parent=sec)
+        output = self._get_captured_output()
+        self.assertEqual(output, [])
+
+        # -- Test cardinality validation warnings on cardinality updates
+        # Test warning when setting minimally required values cardinality
+        prop_card.val_cardinality = (3, None)
+        output = self._get_captured_output()
+        test_msg = "%s (minimum %s values, %s found)" % (self.msg_base, 3, 2)
+        self.assertEqual(len(output), 1)
+        self.assertIn(test_msg, output[0])
+
+        # Test warning when setting maximally required values cardinality
+        prop_card.values = [1, 2, 3]
+        prop_card.val_cardinality = 2
+        output = self._get_captured_output()
+        test_msg = "%s (maximum %s values, %s found)" % (self.msg_base, 2, 3)
+        self.assertEqual(len(output), 1)
+        self.assertIn(test_msg, output[0])
+
+        # Test no warning on valid cardinality
+        prop_card.val_cardinality = (1, 10)
+        output = self._get_captured_output()
+        self.assertEqual(output, [])
+
+        # Test no warning when setting cardinality to None
+        prop_card.val_cardinality = None
+        output = self._get_captured_output()
+        self.assertEqual(output, [])
+
+        # -- Test cardinality validation warnings on values updates
+        # Test warning when violating minimally required values cardinality
+        prop_card.val_cardinality = (3, None)
+        prop_card.values = [1, 2]
+        output = self._get_captured_output()
+        test_msg = "%s (minimum %s values, %s found)" % (self.msg_base, 3, 2)
+        self.assertEqual(len(output), 1)
+        self.assertIn(test_msg, output[0])
+
+        # Test warning when violating maximally required values cardinality
+        prop_card.val_cardinality = (None, 2)
+        prop_card.values = [1, 2, 3]
+        output = self._get_captured_output()
+        test_msg = "%s (maximum %s values, %s found)" % (self.msg_base, 2, 3)
+        self.assertEqual(len(output), 1)
+        self.assertIn(test_msg, output[0])
+
+        # Test no warning when setting correct number of values
+        prop_card.values = [1, 2]
+        output = self._get_captured_output()
+        self.assertEqual(output, [])
+
+        # Test no warning when setting values to None
+        prop_card.values = None
+        output = self._get_captured_output()
+        self.assertEqual(output, [])
