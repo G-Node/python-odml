6a9f3f76f0cf96d98bc6c72152f1e8dc51d6288c
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-04 20:06:05 +0200

    [test/validation] Add prop values cardinality test

--- test/test_validation.py
+++ test/test_validation.py
@@ -41,6 +41,49 @@ class TestValidation(unittest.TestCase):
                 return
         self.assertEqual(errs, err)
 
+    def test_property_values_cardinality(self):
+        doc = odml.Document()
+        sec = odml.Section(name="sec", type="sec_type", parent=doc)
+
+        # Test no caught warning on empty cardinality
+        prop = odml.Property(name="prop_empty_cardinality", values=[1, 2, 3, 4], parent=sec)
+        # Check that the current property is not in the list of validation warnings or errors
+        for err in validate(doc).errors:
+            self.assertNotEqual(err.obj.id, prop.id)
+
+        # Test no warning on valid cardinality
+        prop = odml.Property(name="prop_valid_cardinality", values=[1, 2, 3, 4],
+                             val_cardinality=(2, 10), parent=sec)
+        for err in validate(doc).errors:
+            self.assertNotEqual(err.obj.id, prop.id)
+
+        # Test minimum value cardinality validation
+        test_val = [1, 2, 3]
+        test_card = 2
+
+        prop = odml.Property(name="prop_invalid_max_val", values=test_val,
+                             val_cardinality=test_card, parent=sec)
+
+        test_msg_base = "Property values cardinality violated"
+        test_msg = "%s (maximum %s values, %s found)" % (test_msg_base, test_card, len(prop.values))
+        for err in validate(doc).errors:
+            if err.obj.id == prop.id:
+                self.assertFalse(err.is_error)
+                self.assertIn(test_msg, err.msg)
+
+        # Test maximum value cardinality validation
+        test_val = "I am a nice text to test"
+        test_card = (4, None)
+
+        prop = odml.Property(name="prop_invalid_min_val", values=test_val,
+                             val_cardinality=test_card, parent=sec)
+
+        test_msg = "%s (minimum %s values, %s found)" % (test_msg_base, test_card[0], len(prop.values))
+        for err in validate(doc).errors:
+            if err.obj.id == prop.id:
+                self.assertFalse(err.is_error)
+                self.assertIn(test_msg, err.msg)
+
     def test_section_type(self):
         doc = samplefile.parse("""s1[undefined]""")
         res = validate(doc)
