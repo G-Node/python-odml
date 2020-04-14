528706598f264d41ba52957a7a9802b0fb4dc07c
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-14 16:38:25 +0200

    [test/util] Add format_cardinality test

---
+++ test/test_util.py
@@ -0,0 +1,53 @@
+"""
+This file tests odml util functions.
+"""
+
+import unittest
+
+from odml.util import format_cardinality
+
+
+class TestUtil(unittest.TestCase):
+
+    def test_format_cardinality(self):
+        # Test empty set
+        self.assertIsNone(format_cardinality(None))
+        self.assertIsNone(format_cardinality([]))
+        self.assertIsNone(format_cardinality({}))
+        self.assertIsNone(format_cardinality(""))
+        self.assertIsNone(format_cardinality(()))
+        self.assertIsNone(format_cardinality((None, None)))
+
+        # Test single int max set
+        self.assertEqual(format_cardinality(10), (None, 10))
+
+        # Test tuple set
+        set_val = (2, None)
+        self.assertEqual(format_cardinality(set_val), set_val)
+        set_val = (None, 2)
+        self.assertEqual(format_cardinality(set_val), set_val)
+        set_val = (2, 3)
+        self.assertEqual(format_cardinality(set_val), set_val)
+
+        # Test set failures
+        with self.assertRaises(ValueError):
+            format_cardinality("a")
+
+        with self.assertRaises(ValueError):
+            format_cardinality(-1)
+
+        with self.assertRaises(ValueError):
+            format_cardinality((1, "b"))
+
+        with self.assertRaises(ValueError):
+            format_cardinality((1, 2, 3))
+
+        with self.assertRaises(ValueError):
+            format_cardinality((-1, 1))
+
+        with self.assertRaises(ValueError):
+            format_cardinality((1, -5))
+
+        with self.assertRaises(ValueError) as exc:
+            format_cardinality((5, 1))
+            self.assertIn("Minimum larger than maximum ", str(exc))
