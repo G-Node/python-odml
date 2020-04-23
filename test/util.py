0c257c945a5e362aaa3d4949d61af5b014413ab0
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-23 12:25:31 +0200

    [test/util] Add central test directory

--- test/util.py
+++ test/util.py
@@ -5,3 +5,21 @@ import os
 import tempfile
 
 ODML_CACHE_DIR = os.path.join(tempfile.gettempdir(), "odml.cache")
+
+TEST_TEMP_DIR = os.path.join(tempfile.gettempdir(), "odml_test")
+if not os.path.exists(TEST_TEMP_DIR):
+    os.mkdir(TEST_TEMP_DIR)
+
+
+def create_test_dir(script_name):
+    """
+    Takes the name of a test script and creates a like-named directory
+    in the main test directory if it does not yet exist.
+
+    :param script_name: String that will be used to create the test directory
+
+    :return: Path to the test directory.
+    """
+    dir_name = "_%s" % os.path.basename(os.path.splitext(script_name)[0])
+
+    return tempfile.mkdtemp(suffix=dir_name, dir=TEST_TEMP_DIR)
