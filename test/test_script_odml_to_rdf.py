ae1f00750af2dccd3d990d7c1931ce8d2d0b99bb
M. Sonntag <michael.p.sonntag@gmail.com>
2020-06-24 15:46:42 +0200

    [test/scripts] Add odmltordf basic test

--- test/test_script_odml_to_rdf.py
+++ test/test_script_odml_to_rdf.py
@@ -3,6 +3,7 @@ import shutil
 import unittest
 
 from docopt import DocoptExit
+from rdflib import Graph
 
 from odml.scripts import odml_to_rdf
 
@@ -13,6 +14,7 @@ class TestScriptOdmlToRDF(unittest.TestCase):
 
     def setUp(self):
         self.tmp_dir = util.create_test_dir(__file__)
+        self.dir_files = os.path.join(util.TEST_RESOURCES_DIR, "scripts", "odml_to_rdf")
 
     def tearDown(self):
         if self.tmp_dir and os.path.exists(self.tmp_dir):
@@ -30,3 +32,31 @@ class TestScriptOdmlToRDF(unittest.TestCase):
 
         with self.assertRaises(SystemExit):
             odml_to_rdf.main(["--version"])
+
+    def test_valid_conversion(self):
+        # make sure temp dir is empty
+        self.assertListEqual(os.listdir(self.tmp_dir), [])
+
+        # run converter on root directory containing two files
+        odml_to_rdf.main(["-o", self.tmp_dir, self.dir_files])
+
+        # make sure an odml version conversion output directory has been created
+        out_dir_lst = os.listdir(self.tmp_dir)
+        self.assertEqual(len(out_dir_lst), 1)
+        out_dir = os.path.join(self.tmp_dir, out_dir_lst[0])
+        self.assertTrue(os.path.isdir(out_dir))
+
+        # make sure an rdf conversion output directory has been created
+        rdf_dir_lst = os.listdir(out_dir)
+        self.assertEqual(len(rdf_dir_lst), 1)
+        rdf_dir = os.path.join(out_dir, rdf_dir_lst[0])
+        self.assertTrue(os.path.isdir(rdf_dir))
+
+        # make sure two files have been created
+        file_lst = os.listdir(rdf_dir)
+        self.assertEqual(len(file_lst), 2)
+
+        # make sure the files are valid RDF files
+        curr_graph = Graph()
+        curr_graph.parse(os.path.join(rdf_dir, file_lst[0]))
+        curr_graph.parse(os.path.join(rdf_dir, file_lst[1]))
