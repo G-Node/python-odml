bf5818743419cd4680e5d6b6de82f4ca47e4bbe1
M. Sonntag <michael.p.sonntag@gmail.com>
2020-06-24 15:49:27 +0200

    [test/scripts] Add odmltordf cli args tests
    Added tests
    - for invalid files
    - for recursive directories

--- test/test_script_odml_to_rdf.py
+++ test/test_script_odml_to_rdf.py
@@ -15,6 +15,8 @@ class TestScriptOdmlToRDF(unittest.TestCase):
     def setUp(self):
         self.tmp_dir = util.create_test_dir(__file__)
         self.dir_files = os.path.join(util.TEST_RESOURCES_DIR, "scripts", "odml_to_rdf")
+        self.dir_invalid = os.path.join(self.dir_files, "test_invalid")
+        self.dir_recursive = os.path.join(self.dir_files, "test_recursive")
 
     def tearDown(self):
         if self.tmp_dir and os.path.exists(self.tmp_dir):
@@ -39,6 +41,56 @@ class TestScriptOdmlToRDF(unittest.TestCase):
 
         # run converter on root directory containing two files
         odml_to_rdf.main(["-o", self.tmp_dir, self.dir_files])
+
+        # make sure an odml version conversion output directory has been created
+        out_dir_lst = os.listdir(self.tmp_dir)
+        self.assertEqual(len(out_dir_lst), 1)
+        out_dir = os.path.join(self.tmp_dir, out_dir_lst[0])
+        self.assertTrue(os.path.isdir(out_dir))
+
+        # make sure an rdf conversion output directory has been created
+        rdf_dir_lst = os.listdir(out_dir)
+        self.assertEqual(len(rdf_dir_lst), 1)
+        rdf_dir = os.path.join(out_dir, rdf_dir_lst[0])
+        self.assertTrue(os.path.isdir(rdf_dir))
+
+        # make sure two files have been created
+        file_lst = os.listdir(rdf_dir)
+        self.assertEqual(len(file_lst), 2)
+
+        # make sure the files are valid RDF files
+        curr_graph = Graph()
+        curr_graph.parse(os.path.join(rdf_dir, file_lst[0]))
+        curr_graph.parse(os.path.join(rdf_dir, file_lst[1]))
+
+    def test_invalid(self):
+        # make sure temp dir is empty
+        self.assertListEqual(os.listdir(self.tmp_dir), [])
+
+        # run converter on directory with invalid files
+        odml_to_rdf.main(["-o", self.tmp_dir, self.dir_invalid])
+
+        # make sure an output directory has been created
+        out_dir_lst = os.listdir(self.tmp_dir)
+        self.assertEqual(len(out_dir_lst), 1)
+        out_dir = os.path.join(self.tmp_dir, out_dir_lst[0])
+        self.assertTrue(os.path.isdir(out_dir))
+
+        # make sure an rdf conversion output directory has been created
+        rdf_dir_lst = os.listdir(out_dir)
+        self.assertEqual(len(rdf_dir_lst), 1)
+        rdf_dir = os.path.join(out_dir, rdf_dir_lst[0])
+        self.assertTrue(os.path.isdir(rdf_dir))
+
+        # make sure no file has been created
+        self.assertListEqual(os.listdir(rdf_dir), [])
+
+    def test_recursive_conversion(self):
+        # make sure temp dir is empty
+        self.assertListEqual(os.listdir(self.tmp_dir), [])
+
+        # run converter on root directory containing two files
+        odml_to_rdf.main(["-r", "-o", self.tmp_dir, self.dir_recursive])
 
         # make sure an odml version conversion output directory has been created
         out_dir_lst = os.listdir(self.tmp_dir)
