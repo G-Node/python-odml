d15a09794a8c10f5b440c5f510eae9816994464f
M. Sonntag <michael.p.sonntag@gmail.com>
2020-07-09 17:34:21 +0200

    [test/RDFWriter] Test custom RDF subclassing

--- test/test_rdf_writer.py
+++ test/test_rdf_writer.py
@@ -310,3 +310,47 @@ class TestRDFWriter(unittest.TestCase):
         rdf_writer = RDFWriter([doc], rdf_subclassing=False)
         result = rdf_writer.get_rdf_str()
         self.assertNotIn("odml:Cell", result)
+
+    def test_rdf_custom_subclasses(self):
+        sub_class_term = "cell"
+
+        # Create minimal document
+        doc = odml.Document()
+        _ = odml.Section(name="test_subclassing", type=sub_class_term, parent=doc)
+
+        # Test None dict
+        rdf_writer = RDFWriter([doc], custom_subclasses=None)
+        self.assertIn("odml:Cell", rdf_writer.get_rdf_str())
+
+        # Test invalid dict
+        rdf_writer = RDFWriter([doc], custom_subclasses=["invalid"])
+        self.assertIn("odml:Cell", rdf_writer.get_rdf_str())
+
+        # Test value whitespace
+        invalid_dict = {"type_1": "Class 1", "type_2": "Class 2"}
+        with self.assertRaises(ValueError):
+            _ = RDFWriter([doc], custom_subclasses=invalid_dict)
+
+        # Test custom subclassing
+        type_custom_class = "species"
+        custom_class_dict = {type_custom_class: "Species"}
+
+        doc = odml.Document()
+        _ = odml.Section(name="test_subclassing", type="cell", parent=doc)
+        _ = odml.Section(name="test_custom_subclassing", type=type_custom_class, parent=doc)
+
+        rdf_writer = RDFWriter([doc], custom_subclasses=custom_class_dict)
+        self.assertIn("odml:Cell", rdf_writer.get_rdf_str())
+        self.assertIn("odml:Species", rdf_writer.get_rdf_str())
+
+        # Test custom subclassing overwrite
+        sub_class_type = "cell"
+        custom_class_dict = {sub_class_type: "Neuron"}
+
+        doc = odml.Document()
+        _ = odml.Section(name="test_subclassing", type=sub_class_type, parent=doc)
+
+        with self.assertWarns(UserWarning):
+            rdf_writer = RDFWriter([doc], custom_subclasses=custom_class_dict)
+            self.assertNotIn("odml:Cell", rdf_writer.get_rdf_str())
+            self.assertIn("odml:Neuron", rdf_writer.get_rdf_str())
